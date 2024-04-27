293a82e036cfa288f173fe116c8eadb681c9f16d
Mehul Jariwala <mjariwala98@gmail.com>
2024-04-27 22:03:38 +0530

    fix: theme issue

--- app/providers/ThemeProvider.tsx
+++ app/providers/ThemeProvider.tsx
@@ -4,17 +4,14 @@ import { FC } from "react";
 
 import { CssBaseline, ThemeProvider as MUIThemeProvider } from "@mui/joy";
 import { StyledEngineProvider } from "@mui/joy/styles";
-import { LIGHTTHEME } from "../styles/MuiTheme";
 
 export const ThemeProvider: FC<{ children: React.ReactNode }> = ({
   children,
 }) => {
   return (
     <StyledEngineProvider injectFirst>
-      <MUIThemeProvider theme={LIGHTTHEME}>
-        <CssBaseline enableColorScheme />
-        {children}
-      </MUIThemeProvider>
+      <CssBaseline enableColorScheme />
+      {children}
     </StyledEngineProvider>
   );
 };
